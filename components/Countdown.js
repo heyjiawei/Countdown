bc77fd1d666cd1a5e235bdfa72e0cf2c938e63a9
heyjiawei <jaewei.j.w@gmail.com>
2017-09-10 01:11:31 +0800

    Completed block layout long title truncation and lengthening when screen resizes

--- components/Countdown.js
+++ components/Countdown.js
@@ -48,46 +48,6 @@ class Countdown extends React.Component {
 	}
 }
 
-class InlineBlockLayout extends React.Component {
-	render() {
-		const Style = {
-			timerDiv: {
-				fontFamily: "Roboto, sans-serif",
-				fontSize: "larger"
-			},
-			dayDiv: {
-				width: calculateWidth(this.props.days) + "%",
-				marginRight: "1%",
-				padding: "0em 0.5em",
-				display: "inline-block",
-				lineHeight: "35px",
-				height: "36px",
-				color: "white",
-				backgroundColor: this.props.color
-			},
-			titleDiv: {
-				fontSize: "smaller",
-				opacity: 0.6,
-				display: "inline-block"
-			}
-		}
-
-		return (
-			<div
-				style={Style.timerDiv}
-				onClick={this.props.onClick}
-			>
-				<div style={Style.dayDiv}>
-					<span>{this.props.days}</span>
-				</div>
-				<div style={Style.titleDiv}>
-					<span>{this.props.title}</span>
-				</div>
-			</div>
-		)
-	}
-}
-
 class BlockLayout extends React.Component {
 	constructor(props) {
 		super(props)
@@ -111,10 +71,11 @@ class BlockLayout extends React.Component {
 	}
 
 	handleWindowResize() {
+		if (this.titleSpan.offsetHeight <= this.daysSpan.offsetHeight) {
+			this.manageLengthen(this.daysDiv, this.titleSpan)
+		}
 		if (this.titleSpan.offsetHeight > this.daysSpan.offsetHeight) {
 			this.manageTruncate(this.daysSpan, this.titleSpan)
-		} else {
-			this.manageLengthen(this.daysDiv, this.titleSpan)
 		}
 	}
 
@@ -136,14 +97,18 @@ class BlockLayout extends React.Component {
 	}
 
 	// Lengthen title string to fit within daysDiv container
-	// when screen width expands
+	// when screen width expands.
+	// Only operates on truncated titles. If title was truncated,
+	// expand title span width when the screen width is larger than
+	// title span by 150px
 	manageLengthen(daysDiv, titleSpan) {
 		if (this.state.isTitleTruncated) {
 
 			let truncatedIndex = this.state.title.length
 			let truncatedTitle = this.state.title
+			let pxBeforeTitleSpanWidthExpands = 100
 
-			while (titleSpan.offsetWidth + 150 < daysDiv.offsetWidth) {
+			while (titleSpan.offsetWidth + pxBeforeTitleSpanWidthExpands < daysDiv.offsetWidth) {
 				if (truncatedIndex < this.props.title.length) {
 					truncatedTitle += this.props.title.charAt(truncatedIndex)
 					titleSpan.innerText = truncatedTitle + "..."
@@ -192,6 +157,112 @@ class BlockLayout extends React.Component {
 					>
 					{this.props.title}
 					</span>
+				</div>
+			</div>
+		)
+	}
+}
+
+class InlineBlockLayout extends React.Component {
+	constructor(props) {
+			super(props)
+			this.state = {
+				isTitleTruncated: false
+			}
+			this.handleWindowResize = this.handleWindowResize.bind(this)
+	}
+
+	componentDidMount() {
+		this.handleWindowResize()
+		window.addEventListener("resize", this.handleWindowResize)
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.handleWindowResize)
+	}
+
+	handleWindowResize() {
+		if (this.titleSpan.offsetHeight <= this.daysSpan.offsetHeight) {
+			this.manageLengthen(this.timerDiv, this.titleSpan)
+		}
+		if (this.titleSpan.offsetHeight > this.daysSpan.offsetHeight) {
+			this.manageTruncate(this.daysSpan, this.titleSpan)
+		}
+	}
+
+	manageTruncate(daysSpan, titleSpan) {
+		let truncatedTitle = titleSpan.innerText
+
+		while(titleSpan.offsetHeight > daysSpan.offsetHeight) {
+			truncatedTitle = truncatedTitle.slice(0, -1)
+			titleSpan.innerText = truncatedTitle + "..."
+		}
+
+		this.setState({
+			isTitleTruncated: true,
+			title: truncatedTitle
+		})
+	}
+
+	manageLengthen(timerDiv, titleSpan) {
+		if (this.state.isTitleTruncated) {
+			let truncatedIndex = this.state.title.length
+			let truncatedTitle = this.state.title
+			let daysDivWidth = calculateWidth(this.props.days) + 1
+			let pxBeforeTitleSpanWidthExpands = 100
+
+			while (titleSpan.offsetWidth + pxBeforeTitleSpanWidthExpands < timerDiv.offsetWidth - daysDivWidth) {
+				if (truncatedIndex < this.props.title.length) {
+					truncatedTitle += this.props.title.charAt(truncatedIndex)
+					titleSpan.innerText = truncatedTitle + "..."
+					truncatedIndex++
+				} else {
+					break
+				}
+			}
+
+			this.setState({
+				title: truncatedTitle
+			})
+		}
+	}
+
+	render() {
+		const Style = {
+			timerDiv: {
+				fontFamily: "Roboto, sans-serif",
+				fontSize: "larger",
+				width: "100%",
+				height: "36px"
+			},
+			dayDiv: {
+				width: calculateWidth(this.props.days) + "%",
+				marginRight: "1%",
+				padding: "0em 0.5em",
+				display: "inline-block",
+				lineHeight: "35px",
+				height: "36px",
+				color: "white",
+				backgroundColor: this.props.color
+			},
+			titleDiv: {
+				fontSize: "smaller",
+				opacity: 0.6,
+				display: "inline"
+			}
+		}
+
+		return (
+			<div
+				style={Style.timerDiv}
+				onClick={this.props.onClick}
+				ref={node => this.timerDiv = node}
+			>
+				<div style={Style.dayDiv}>
+					<span ref={node => this.daysSpan = node}>{this.props.days}</span>
+				</div>
+				<div style={Style.titleDiv}>
+					<span ref={node => this.titleSpan = node}>{this.props.title}</span>
 				</div>
 			</div>
 		)
